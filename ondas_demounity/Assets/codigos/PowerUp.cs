339c43b1752bd0aa6703557811ce8b14442eef17
Juan David <juan.orejuela@hotmail.com>
2015-11-29 18:02:23 -0500

    Commit Inicial

---
+++ ondas_demounity/Assets/codigos/PowerUp.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PowerUp : MonoBehaviour {
+
+	HUDJuego hud;
+
+	void OnTriggerEnter2D(Collider2D other)
+	{
+		if (other.tag == "Player") 
+		{
+			hud = GameObject.Find("Main Camera").GetComponent<HUDJuego>();
+			hud.incrementarPuntaje(10);
+			Destroy (this.gameObject);
+		}
+	}
+}
