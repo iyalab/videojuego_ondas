34b52d57ef4df7b40234089cbde74966266b2500
Juan David <juan.orejuela@hotmail.com>
2015-11-29 19:28:16 -0500

    29/11/2015 7:28pm

---
+++ Tutorial_Runner/Assets/codigos/PersonajeControl.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+
+[RequireComponent(typeof(Personaje))]
+public class PersonajeControl : MonoBehaviour 
+{
+	private Personaje personaje;
+    private bool salto;
+	AudioSource as_personaje;
+	public AudioClip ac_jump;
+
+
+	void Awake()
+	{
+		personaje = GetComponent<Personaje>();
+		as_personaje = gameObject.GetComponent<AudioSource>();
+	}
+
+    void Update ()
+    {
+		if (Input.GetButtonDown("Jump")){
+			salto = true;
+			as_personaje.PlayOneShot(ac_jump);
+		} 
+    }
+
+	void FixedUpdate()
+	{
+	    float h = Input.GetAxis("Horizontal");
+		personaje.Move( 1, false , salto );
+		salto = false;
+	}
+}
