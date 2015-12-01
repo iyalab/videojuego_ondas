34f33e3702bc19a49831194908dc64d96c0318ac
Juan David <juan.orejuela@hotmail.com>
2015-12-01 13:52:58 -0500

    Tutorial Runner Ondas 1/dic/2015

---
+++ Assets/codigos/ControlUI.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ControlUI : MonoBehaviour {
+
+	public int escena;
+
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	public void cargarEscena(){
+		Application.LoadLevel(escena);
+	}
+}
--- Assets/codigos/GameOver.cs
+++ Assets/codigos/GameOver.cs
@@ -12,11 +12,15 @@ public class GameOver : MonoBehaviour {
 
 	void OnGUI()
 	{
-		GUI.Label (new Rect (Screen.width / 2 - 40, 50, 80, 30), "FIN DEL JUEGO");
+		GUI.Label (new Rect (Screen.width / 2 - 40, 50, 200, 100), "FIN DEL JUEGO");
 
-		GUI.Label (new Rect (Screen.width / 2 - 40, 300, 80, 30), "Puntaje: " + puntaje);
+		GUI.Label (new Rect (Screen.width / 2 - 40, 300, 200, 100), "Puntaje: " + puntaje);
 
-		if (GUI.Button (new Rect (Screen.width / 2 - 30, 350, 60, 30), "Otra Vez?")) 
+		if (GUI.Button (new Rect (Screen.width / 2 - 100, 350, 200, 50), "Otra Vez?")) 
+		{
+			Application.LoadLevel(1);
+		}
+		if (GUI.Button (new Rect (Screen.width / 2 - 100, 450, 200, 50), "Ir al inicio")) 
 		{
 			Application.LoadLevel(0);
 		}
